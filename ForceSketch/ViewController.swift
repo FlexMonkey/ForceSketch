6d435ef58bdf87c62863cc6f64e172f45f004179
simon gladman <sgladman@gmail.com>
2015-10-07 10:37:52 +0100

    Initial Commit

---
+++ ForceSketch/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ForceSketch
+//
+//  Created by SIMON_NON_ADMIN on 07/10/2015.
+//  Copyright © 2015 Simon Gladman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
